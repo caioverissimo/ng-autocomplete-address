01e200d787b058fe79f6310f5afff49b651ae48a
Caio Verissimo Barbosa <cbarbosa.ext@vr.com.br>
2016-06-08 14:54:55 -0300

    create first tests for return a callback in async

--- app/scripts/main - Copia.js
+++ app/scripts/main - Copia.js
@@ -1,36 +1,19 @@
 (function () {
   'use strict';
   angular
-      .module('autocompleteDemo', ['ngMaterial'])
-      .controller('DemoCtrl', DemoCtrl);
-  function DemoCtrl ($timeout, $q, $log, $http) {
-  	var contador = 0;
+  	.module('autocompleteDemo', ['ngMaterial'])
+  	.controller('DemoCtrl', DemoCtrl);
+
+  function DemoCtrl ($timeout, $q, $log) {
     var self = this;
     self.simulateQuery = false;
     self.isDisabled    = false;
     // list of `state` value/display objects
-    // self.states        = loadAll();
+    self.states        = loadAll();
     self.querySearch   = querySearch;
     self.selectedItemChange = selectedItemChange;
     self.searchTextChange   = searchTextChange;
     self.newState = newState;
-
-    function checkApi (addressValue, callback) {
-      $http({
-        method: 'GET',
-        url: 'http://maps.googleapis.com/maps/api/geocode/json?address='+addressValue
-      }).success(function (data, status, headers, config) {
-        // $scope.resposta = data;
-        console.log("###data: ");
-        console.log(data.results);        
-        callback(data.results);
-
-      }).error(function(data, status, headers, config){
-        // $scope.erro = data;
-      });
-    };
-
-
     function newState(state) {
       alert("Sorry! You'll need to create a Constituion for " + state + " first!");
     }
@@ -42,11 +25,6 @@
      * remote dataservice call.
      */
     function querySearch (query) {
-    	self.states = loadAll();
-      console.log(query)
-      console.log("#######wowRes: ");
-      checkApi( query, function(resultado) { return console.log('abii: '), console.log(resultado) } ) ;
-      
       var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
           deferred;
       if (self.simulateQuery) {
@@ -67,9 +45,6 @@
      * Build `states` list of key/value pairs
      */
     function loadAll() {
-    	contador++;
-    	console.log(contador);
-			console.log('loadAll');
       var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
               Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
               Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
@@ -94,4 +69,5 @@
       };
     }
   }
+
 })();
